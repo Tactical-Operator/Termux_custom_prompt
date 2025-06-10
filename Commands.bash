2f3f9a1051616ebd53566271f4185cb083e252cf
ASHWIN JADHAV <133649468+Tactical-Operator@users.noreply.github.com>
2025-06-10 19:45:28 +0530

    Update Commands.bash

--- Commands.bash
+++ Commands.bash
@@ -1,5 +1,6 @@
 # If you want to know only the commands , glad you are here , please follow all the correct steps
 
+termux-setup-storage # give the persmissions /if already done follow the next steps.
 
 
 
