a2c6c8545ce467b4a8d6bcc11877fd0319c32f1f
ASHWIN JADHAV <133649468+Tactical-Operator@users.noreply.github.com>
2025-06-12 13:40:39 +0530

    Update Commands.bash

--- Commands.bash
+++ Commands.bash
@@ -1,6 +1,8 @@
 # If you want to know only the commands , glad you are here , please follow all the correct steps
+# Assuming that you have already downloaded the font file and saved it in storage.
 
 termux-setup-storage # give the persmissions /if already done follow the next steps.
+cd storage/downloads
 
 
 
