1b5d4b6746b10a91e8ecf825595ff15f4dd7b83f
ASHWIN JADHAV <133649468+Tactical-Operator@users.noreply.github.com>
2025-06-12 13:44:41 +0530

    Update Commands.bash

--- Commands.bash
+++ Commands.bash
@@ -2,7 +2,13 @@
 # Assuming that you have already downloaded the font file and saved it in storage.
 
 termux-setup-storage # give the persmissions /if already done follow the next steps.
-cd storage/downloads
+cd storage/downloads # check your file JetBrainsMonoNerdFontMono-Regular.ttf ,you can shoose other font if you want.
+
+# come back to the starting directory 
+mkdir -p ~/.termux # here we will store our JetBrainsMonoNerdFontMono-Regular.ttf file.
+cp ~/storage/downloads/JetBrainsMonoNerdFontMono-Regular.ttf ~/.termux/font.ttf #copying the file to termux directory.
+
+
 
 
 
